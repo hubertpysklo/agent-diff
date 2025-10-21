d00a41db0af3e495047fd5bbe9eec94589597606
hubertpysklo <hubert@uni.minerva.edu>
2025-10-21 18:38:21 -0300

    fix

--- sdk/agent_diff/__init__.py
+++ sdk/agent_diff/__init__.py
@@ -1,5 +1,4 @@
-from .client import DiffAgent
-from .models import Environment, TestRun, Diff
+from .client import AgentDiff
 
 __version__ = "0.0.1"
-__all__ = ["DiffAgent", "Environment", "TestRun", "Diff"]
+__all__ = ["AgentDiff"]
--- sdk/agent_diff/client.py
+++ sdk/agent_diff/client.py
@@ -2,26 +2,29 @@ import os
 import requests
 from models import InitEnvRequestBody, InitEnvResponse
 
+## WIP
+
 
 class AgentDiff:
     def __init__(self, api_key: str | None = None, base_url: str | None = None):
-        self.api_key = api_key or os.getenv("DIFF_AGENT_API_KEY")
+        self.api_key = api_key or os.getenv("AGENT_DIFF_API_KEY")
         self.base_url = base_url or os.getenv(
-            "DIFF_AGENT_BASE_URL",
+            "AGENT_DIFF_BASE_URL",
         )
         if not self.api_key:
             raise ValueError(
-                "API key required. Set DIFF_AGENT_API_KEY env var or pass api_key parameter"
+                "API key required. Set AGENT_DIFF_API_KEY env var or pass api_key parameter"
             )
         if not self.base_url:
             raise ValueError(
-                "Base URL required. Set DIFF_AGENT_BASE_URL env var or pass base_url parameter"
+                "Base URL required. Set AGENT_DIFF_BASE_URL env var or pass base_url parameter"
             )
+
     def init_env(self, request: InitEnvRequestBody) -> InitEnvResponse:
         response = requests.post(
             f"{self.base_url}/api/platform/initEnv",
             json=request.model_dump(mode="json"),
-            headers={"Authorization": f"ApiKey {self.api_key}"},
+            headers={"X-API-Key": self.api_key},
             timeout=30,
         )
         response.raise_for_status()
@@ -46,6 +49,7 @@ class AgentDiff:
         pass
 
     def create_test_suite(self, test_suite: TestSuite) -> TestSuite:
+        pass
 
     def take_before_snapshot(self, env_id: str) -> BeforeSnapshotResponse:
         pass
@@ -56,7 +60,9 @@ class AgentDiff:
     def get_diff(self, before_suffix: str, after_suffix: str) -> Diff:
         pass  # This function should accept aither a runId or just pure test written in DSL
 
-    def evaluate(self, before_suffix: str, after_suffix: str, expected_output: dict) -> Evaluation:
+    def evaluate(
+        self, before_suffix: str, after_suffix: str, expected_output: dict
+    ) -> Evaluation:
         pass  # This function should accept aither a runId or just pure test written in DSL
 
     def start_run(self, run: Run) -> Run:
@@ -70,5 +76,3 @@ class AgentDiff:
 
     def delete_env(self, env_id: str) -> DeleteEnvResponse:
         pass
-
-
